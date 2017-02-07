0bd7d7c886b4cb3bc5b3e46510b0c01a2e891e39
levon Hambardzumyan <lev.hambardzumyan@gmail.com>
2017-02-08 00:50:06 +0400

    first commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,112 @@
+<head>
+    <link rel="stylesheet" href="../public/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../public/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../public/bootstrap/css/style.css">
+    <script type="text/javascript" src="../public/bootstrap/js/jquery-1.11.3.min.js"></script>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <meta charset="utf-8">
+    <title>Welcome to CodeIgniter</title>
+
+
+</head>
+
+<div class="container-fluid">
+    <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+
+    <!-- Columns are always 50% wide, on mobile and desktop -->
+    <div class="row">
+        <div class="cover col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+            <div class="row" >
+                <div class=" col-lg-4 col-lg-offset-1  col-md-4 col-md-offset-1 col-sm-4 col-sm-offset-1 col-xs-4 col-xs-offset-1">
+                   <div class="logo">
+                    <img src="../public/bootstrap/img/logo.png"
+                         height="130" width="130" style="vertical-align:middle;">
+                </div>
+                <h2 style="color:rgb(178, 25, 25);font-weight: bold;font-size:140%;">WELCOME TO OUR</h2>
+                <h1 style="color:rgb(178, 25, 25);font-weight: bold;font-size:400%;margin-top: -17px;">SCHOOL</h1>
+                <h2 style="color:rgb(198, 167, 41);font-weight: bold;font-size:220%;margin-top: -7px;width:103%">DAVID FAHRADYAN</h2>
+                <p style="width:85%;margin-top: -14px;color:#262626;">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of
+                    type and scrambled it to make a type
+                </p>
+                    <div style="background:red; width: 72%;height: 12%;">
+                    <button type="button" style="background: rgb(198, 167, 41);color: white;width: 100%;height: 95%;font-size: 200%;">see more...</button>
+                        </div>
+                </div>
+                <div class="col-lg-1 col-lg-offset-1 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1 col-xs-5 col-xs-offset-1" style=" margin-top:1%">
+                    <img src="../public/bootstrap/img/dav_fah.png" height="500" width="400">
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row">
+        <div class="red col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="row" style="margin-top:-7%">
+                <div class="col-lg-3 col-lg-offset-1 col-md-3 col-md-offset-1 col-sm-3  col-sm-offset-1 col-xs-3 col-xs-offset-1">
+                    <img src="../public/bootstrap/img/school.png"
+                         height="180" width="280" style="">
+                    <h1 style="color:white;font-weight: bold;font-size:200%;">SCHOOL</h1>
+                    <p style="width:100%;color:white;">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been
+                    </p>
+                    <div style="; width: 40%;height: 8%;">
+                        <button type="button" style="background: white;color: black;width: 176%;height: 89%;font-size: 200%;">see more...</button>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 " style="margin-left: 3%">
+                    <img src="../public/bootstrap/img/bio.png"
+                         height="180" width="280" style="">
+                    <h1 style="color:white;font-weight: bold;font-size:200%;">BIOGRAPHY</h1>
+                    <p style="width:100%;color:white;">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been
+                    </p>
+                    <div style="; width: 40%;height: 8%;">
+                    <button type="button" style="background: white;color: black;width: 176%;height: 89%;font-size: 200%;">see more...</button>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 "style="margin-left: 5%" >
+                <img src="../public/bootstrap/img/records.png"
+                     height="180" width="280" style="">
+                <h1 style="color:white;font-weight: bold;font-size:200%;">RECORDS</h1>
+                <p style="width:100%;color:white;">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been
+                </p>
+                <div style="; width: 40%;height: 8%;">
+                    <button type="button" style="background: white;color: black;width: 176%;height: 89%;font-size: 200%;">see more...</button>
+                </div>
+                </div>
+                </div>
+    </div>
+    </div>
+
+    <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+    <div class="row">
+        <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+        <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+        <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+    </div>
+
+</div>
+
+<script type="text/javascript" src="../public/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../public/bootstrap/js/jquery.js"></script>
