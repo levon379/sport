00e713119a6659c62ea2a30445dfd10c67d506e8
levon Hambardzumyan <lev.hambardzumyan@gmail.com>
2017-02-08 21:45:32 +0400

    2 commit

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -18,26 +18,33 @@
     <div class="row">
         <div class="cover col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
-            <div class="row" >
+            <div class="row">
                 <div class=" col-lg-4 col-lg-offset-1  col-md-4 col-md-offset-1 col-sm-4 col-sm-offset-1 col-xs-4 col-xs-offset-1">
-                   <div class="logo">
-                    <img src="../public/bootstrap/img/logo.png"
-                         height="130" width="130" style="vertical-align:middle;">
-                </div>
-                <h2 style="color:rgb(178, 25, 25);font-weight: bold;font-size:140%;">WELCOME TO OUR</h2>
-                <h1 style="color:rgb(178, 25, 25);font-weight: bold;font-size:400%;margin-top: -17px;">SCHOOL</h1>
-                <h2 style="color:rgb(198, 167, 41);font-weight: bold;font-size:220%;margin-top: -7px;width:103%">DAVID FAHRADYAN</h2>
-                <p style="width:85%;margin-top: -14px;color:#262626;">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of
-                    type and scrambled it to make a type
-                </p>
+                    <div class="logo">
+                        <img src="../public/bootstrap/img/logo.png"
+                             height="130" width="130" style="vertical-align:middle;">
+                    </div>
+                    <h2 style="color:rgb(178, 25, 25);font-weight: bold;font-size:140%;">WELCOME TO OUR</h2>
+                    <h1 style="color:rgb(178, 25, 25);font-weight: bold;font-size:400%;margin-top: -17px;">SCHOOL</h1>
+                    <h2 style="color:rgb(198, 167, 41);font-weight: bold;font-size:220%;margin-top: -7px;width:103%">
+                        DAVID FAHRADYAN</h2>
+                    <p style="width:85%;margin-top: -14px;color:#262626;">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of
+                        type and scrambled it to make a type
+                    </p>
                     <div style="background:red; width: 72%;height: 12%;">
-                    <button type="button" style="background: rgb(198, 167, 41);color: white;width: 100%;height: 95%;font-size: 200%;">see more...</button>
-                        </div>
+                        <button type="button"
+                                style="background: rgb(198, 167, 41);color: white;width: 100%;height: 95%;font-size: 200%;">
+                            see more...
+                        </button>
+<!--                        <a href="" class="btn" style="width:100%;font-size:128%;background: rgb(198, 167, 41);color:white;border-radius:0px;">-->
+<!--                            see more...</a>-->
+                    </div>
                 </div>
-                <div class="col-lg-1 col-lg-offset-1 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1 col-xs-5 col-xs-offset-1" style=" margin-top:1%">
+                <div class="col-lg-5 col-lg-offset-1 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1 col-xs-5 col-xs-offset-1"
+                    style=" margin-top:1%">
                     <img src="../public/bootstrap/img/dav_fah.png" height="500" width="400">
                 </div>
 
@@ -45,66 +52,208 @@
         </div>
 
     </div>
-
-    <div class="row">
-        <div class="red col-lg-12 col-md-12 col-sm-12 col-xs-12">
+<!-- bio awards shcool start-->
+    <div class="row red">
             <div class="row" style="margin-top:-7%">
-                <div class="col-lg-3 col-lg-offset-1 col-md-3 col-md-offset-1 col-sm-3  col-sm-offset-1 col-xs-3 col-xs-offset-1">
-                    <img src="../public/bootstrap/img/school.png"
-                         height="180" width="280" style="">
-                    <h1 style="color:white;font-weight: bold;font-size:200%;">SCHOOL</h1>
-                    <p style="width:100%;color:white;">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum has been
-                    </p>
-                    <div style="; width: 40%;height: 8%;">
-                        <button type="button" style="background: white;color: black;width: 176%;height: 89%;font-size: 200%;">see more...</button>
+                <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10  col-sm-offset-1 col-xs-10 col-xs-offset-1">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                            <img src="../public/bootstrap/img/school.png"
+                                 height="180" width="100%" style="">
+                            <h1 style="color:white;font-weight: bold;font-size:200%;">SCHOOL</h1>
+                            <p style="width:100%;color:white;">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been
+                            </p>
+                            <a href="" class="btn" style="width:48%;font-size:128%;background-color:white;color:#272727;border-radius:0px;">
+                                see more...</a>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                            <img src="../public/bootstrap/img/bio.png"
+                                 height="180" width="100%" style="">
+                            <h1 style="color:white;font-weight: bold;font-size:200%;">BIOGRAPHY</h1>
+                            <p style="width:100%;color:white;">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been
+                            </p>
+                            <a href="" class="btn" style="width:48%;font-size:128%;background-color:white;color:#272727;border-radius:0px;">
+                                see more...</a>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                            <img src="../public/bootstrap/img/records.png"
+                                 height="180" width="100%" style="">
+                            <h1 style="color:white;font-weight: bold;font-size:200%;">RECORDS</h1>
+                            <p style="width:100%;color:white;">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been
+                            </p>
+                            <a href="" class="btn" style="width:48%;font-size:128%;background-color:white;color:#272727;border-radius:0px;">
+                                see more...</a>
+                        </div>
                     </div>
+
+
+
                 </div>
 
-                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 " style="margin-left: 3%">
-                    <img src="../public/bootstrap/img/bio.png"
-                         height="180" width="280" style="">
-                    <h1 style="color:white;font-weight: bold;font-size:200%;">BIOGRAPHY</h1>
-                    <p style="width:100%;color:white;">
+            </div>
+
+    </div>
+    <!-- bio awards shcool end-->
+    <!-- video galery start -->
+    <div class="row" style="height:400px;background-color:white">
+        <div style="border-bottom:2px solid #B21919;" class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10  col-sm-offset-1 col-xs-10 col-xs-offset-1">
+            <div class="row">
+                <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5 ">
+                <div class="row" style="height:400px;background-color:white">
+            <h1 style="color:rgb(91, 10, 10);font-weight: bold;font-size:300%;">LOREM ISPUMN</h1>
+            <h1 style="font-weight: bold;font-size:150%;margin-top: -10px;">There are many</h1>
+            <h1 style="font-weight: bold;font-size:150%;margin-top: -10px;">variations of passages</h1>
+            <p style="width:100%;">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been
+            </p>
+            <a href="" class="btn " style="background-color:white;color:#B21919;border-radius:0px;border-color:#B21919">VIEW
+                ALL VIEDEOS</a>
+                    </div>
+                    </div>
+        <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">video slider </div>
+        </div>
+
+    </div>
+        <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 "> </div>
+    </div>
+    <!-- video galery end -->
+    <!-- photo galery start -->
+    <div style="clear:both"></div>
+    <h1 class="text-center">PHOTO GALERY</h1>
+    <div class="row" style="height:210px;background-color:white">
+        <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10  col-sm-offset-1 col-xs-10 col-xs-offset-1">
+           <div class="row">
+               <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 ">
+                   <img src="../public/bootstrap/img/records.png" height="200" width="100%">
+               </div>
+               <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 ">
+                   <img src="../public/bootstrap/img/records.png" height="200" width="100%">
+               </div>
+           </div>
+
+
+        </div>
+        <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 "> </div>
+
+    </div>
+    <div class="row" style="height:250px;background-color:white">
+        <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10  col-sm-offset-1 col-xs-10 col-xs-offset-1">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <img src="../public/bootstrap/img/records.png" height="220" width="">
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <img src="../public/bootstrap/img/records.png" height="220" width="" >
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <img src="../public/bootstrap/img/records.png" height="220" width="">
+                </div>
+            </div>
+
+
+
+        </div>
+
+        <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 "> </div>
+
+    </div>
+    <!-- photo galery end -->
+    <!-- our news start -->
+    <div class="row grad " style="height:420px;">
+
+        <!--            <img src="../public/bootstrap/img/grad_cover.png" />-->
+        <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10  col-sm-offset-1 col-xs-10 col-xs-offset-1">
+            <span style="font-weight: bold;font-size:200%;color:white">OUR NEWS</span><a href=""> <span
+                    style="color:white;margin-left:3%;font-weight: bold;font-size:140%;">View all</span></a>
+            <div class="row" style="border-bottom: 1px solid white;">
+                <div class=" col-lg-4 col-md-4 col-sm-6 col-xs-12 ">
+                    <p>
+                    <img src="../public/bootstrap/img/records.png" width="50%" height="40%" align="left"/>
+                    <h1 style="color:white;font-weight: bold;font-size:150%;margin-top: -7px;width:103%">NEWS NAME</h1>
+                    <h1 style="color:rgb(198, 167, 41);font-size:150%;margin-top: -7px;width:103%">01.01.17</h1>
+                            <span style="color:white">
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                         Lorem Ipsum has been
+                            </span>
                     </p>
-                    <div style="; width: 40%;height: 8%;">
-                    <button type="button" style="background: white;color: black;width: 176%;height: 89%;font-size: 200%;">see more...</button>
-                    </div>
+                    <h1 style="font-size:150%;color:white">View more</h1>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 "style="margin-left: 5%" >
-                <img src="../public/bootstrap/img/records.png"
-                     height="180" width="280" style="">
-                <h1 style="color:white;font-weight: bold;font-size:200%;">RECORDS</h1>
-                <p style="width:100%;color:white;">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <p>
+                    <img src="../public/bootstrap/img/records.png" width="50%" height="40%" align="left"/>
+                    <h1 style="color:white;font-weight: bold;font-size:150%;margin-top: -7px;width:103%">NEWS NAME</h1>
+                    <h1 style="color:rgb(198, 167, 41);font-size:150%;margin-top: -7px;width:103%">01.01.17</h1>
+                            <span style="color:white">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been
+                            </span>
+                    </p>
+                    <h1 style="font-size:150%;color:white">View more</h1>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <p>
+                    <img src="../public/bootstrap/img/records.png" width="50%" height="40%" align="left"/>
+                    <h1 style="color:white;font-weight: bold;font-size:150%;margin-top: -7px;width:103%">NEWS NAME</h1>
+                    <h1 style="color:rgb(198, 167, 41);font-size:150%;margin-top: -7px;width:103%">01.01.17</h1>
+                            <span style="color:white">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been
+                            </span>
+                    </p>
+                    <h1 style="font-size:150%;color:white">View more</h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 "> </div>
+<!--footer start-->
+
+        <div class="row" >
+
+            <div style="margin-top:10px;" class="col-lg-3 col-lg-offset-1 col-md-3 col-md-offset-1 col-sm-3  col-sm-offset-1 col-xs-3 col-xs-offset-1">
+                <p><img src="../public/bootstrap/img/phone.png" height="28" width="28" align="left" />
+                <span style="color:white;font-size:140%;">+374 91 00 00 00</span>
                 </p>
-                <div style="; width: 40%;height: 8%;">
-                    <button type="button" style="background: white;color: black;width: 176%;height: 89%;font-size: 200%;">see more...</button>
-                </div>
-                </div>
-                </div>
-    </div>
-    </div>
+            </div>
+            <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5 "> </div>
+            <div style="margin-top:10px;" class="col-lg-2 col-md-2 col-sm-2 col-xs-2 ">
+                <span style="color:white;font-size:83%;">design and developmant by Astudio</span>
+            </div>
 
-    <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
-    <div class="row">
-        <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-        <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-        <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 "> </div>
+        </div>
+            <!--footer end-->
+
     </div>
+    <!-- our news end -->
 
 </div>
 
