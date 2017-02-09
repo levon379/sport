78ead0c05896ad444345f387e22c94b40d3aa5d0
levon Hambardzumyan <lev.hambardzumyan@gmail.com>
2017-02-09 10:37:17 +0400

    move non bootstrap files from bootstrap folder

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,7 +1,8 @@
+
 <head>
     <link rel="stylesheet" href="../public/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="../public/bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="../public/bootstrap/css/style.css">
+    <link rel="stylesheet" href="../public/css/style.css">
     <script type="text/javascript" src="../public/bootstrap/js/jquery-1.11.3.min.js"></script>
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
@@ -21,10 +22,10 @@
             <div class="row">
                 <div class=" col-lg-4 col-lg-offset-1  col-md-4 col-md-offset-1 col-sm-4 col-sm-offset-1 col-xs-4 col-xs-offset-1">
                     <div class="logo">
-                        <img src="../public/bootstrap/img/logo.png"
-                             height="130" width="130" style="vertical-align:middle;">
+                        <img src="../public/img/logo.png"
+                             height="130" width="130" >
                     </div>
-                    <h2 style="color:rgb(178, 25, 25);font-weight: bold;font-size:140%;">WELCOME TO OUR</h2>
+                    <h2 class="welcome-title" style="">WELCOME TO OUR</h2>
                     <h1 style="color:rgb(178, 25, 25);font-weight: bold;font-size:400%;margin-top: -17px;">SCHOOL</h1>
                     <h2 style="color:rgb(198, 167, 41);font-weight: bold;font-size:220%;margin-top: -7px;width:103%">
                         DAVID FAHRADYAN</h2>
@@ -45,7 +46,7 @@
                 </div>
                 <div class="col-lg-5 col-lg-offset-1 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1 col-xs-5 col-xs-offset-1"
                     style=" margin-top:1%">
-                    <img src="../public/bootstrap/img/dav_fah.png" height="500" width="400">
+                    <img src="../public/img/dav_fah.png" height="500" width="400">
                 </div>
 
             </div>
@@ -58,7 +59,7 @@
                 <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10  col-sm-offset-1 col-xs-10 col-xs-offset-1">
                     <div class="row">
                         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                            <img src="../public/bootstrap/img/school.png"
+                            <img src="../public/img/school.png"
                                  height="180" width="100%" style="">
                             <h1 style="color:white;font-weight: bold;font-size:200%;">SCHOOL</h1>
                             <p style="width:100%;color:white;">
@@ -72,7 +73,7 @@
                                 see more...</a>
                         </div>
                         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                            <img src="../public/bootstrap/img/bio.png"
+                            <img src="../public/img/bio.png"
                                  height="180" width="100%" style="">
                             <h1 style="color:white;font-weight: bold;font-size:200%;">BIOGRAPHY</h1>
                             <p style="width:100%;color:white;">
@@ -86,7 +87,7 @@
                                 see more...</a>
                         </div>
                         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                            <img src="../public/bootstrap/img/records.png"
+                            <img src="../public/img/records.png"
                                  height="180" width="100%" style="">
                             <h1 style="color:white;font-weight: bold;font-size:200%;">RECORDS</h1>
                             <p style="width:100%;color:white;">
@@ -143,10 +144,10 @@
         <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10  col-sm-offset-1 col-xs-10 col-xs-offset-1">
            <div class="row">
                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 ">
-                   <img src="../public/bootstrap/img/records.png" height="200" width="100%">
+                   <img src="../public/img/records.png" height="200" width="100%">
                </div>
                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 ">
-                   <img src="../public/bootstrap/img/records.png" height="200" width="100%">
+                   <img src="../public/img/records.png" height="200" width="100%">
                </div>
            </div>
 
@@ -159,13 +160,13 @@
         <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10  col-sm-offset-1 col-xs-10 col-xs-offset-1">
             <div class="row">
                 <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                    <img src="../public/bootstrap/img/records.png" height="220" width="">
+                    <img src="../public/img/records.png" height="220" width="">
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                    <img src="../public/bootstrap/img/records.png" height="220" width="" >
+                    <img src="../public/img/records.png" height="220" width="" >
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                    <img src="../public/bootstrap/img/records.png" height="220" width="">
+                    <img src="../public/img/records.png" height="220" width="">
                 </div>
             </div>
 
@@ -187,7 +188,7 @@
             <div class="row" style="border-bottom: 1px solid white;">
                 <div class=" col-lg-4 col-md-4 col-sm-6 col-xs-12 ">
                     <p>
-                    <img src="../public/bootstrap/img/records.png" width="50%" height="40%" align="left"/>
+                    <img src="../public/img/records.png" width="50%" height="40%" align="left"/>
                     <h1 style="color:white;font-weight: bold;font-size:150%;margin-top: -7px;width:103%">NEWS NAME</h1>
                     <h1 style="color:rgb(198, 167, 41);font-size:150%;margin-top: -7px;width:103%">01.01.17</h1>
                             <span style="color:white">
@@ -202,7 +203,7 @@
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                     <p>
-                    <img src="../public/bootstrap/img/records.png" width="50%" height="40%" align="left"/>
+                    <img src="../public/img/records.png" width="50%" height="40%" align="left"/>
                     <h1 style="color:white;font-weight: bold;font-size:150%;margin-top: -7px;width:103%">NEWS NAME</h1>
                     <h1 style="color:rgb(198, 167, 41);font-size:150%;margin-top: -7px;width:103%">01.01.17</h1>
                             <span style="color:white">
@@ -217,7 +218,7 @@
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                     <p>
-                    <img src="../public/bootstrap/img/records.png" width="50%" height="40%" align="left"/>
+                    <img src="../public/img/records.png" width="50%" height="40%" align="left"/>
                     <h1 style="color:white;font-weight: bold;font-size:150%;margin-top: -7px;width:103%">NEWS NAME</h1>
                     <h1 style="color:rgb(198, 167, 41);font-size:150%;margin-top: -7px;width:103%">01.01.17</h1>
                             <span style="color:white">
@@ -239,7 +240,7 @@
         <div class="row" >
 
             <div style="margin-top:10px;" class="col-lg-3 col-lg-offset-1 col-md-3 col-md-offset-1 col-sm-3  col-sm-offset-1 col-xs-3 col-xs-offset-1">
-                <p><img src="../public/bootstrap/img/phone.png" height="28" width="28" align="left" />
+                <p><img src="../public/img/phone.png" height="28" width="28" align="left" />
                 <span style="color:white;font-size:140%;">+374 91 00 00 00</span>
                 </p>
             </div>
